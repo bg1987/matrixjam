66fc9718f744aed79045deded2f7fe23acf5df79
Alon Wolf <alonw125@gmail.com>
2020-08-06 23:27:34 +0300

    fix

--- Assets/Team20/EmptyGame/Scripts/MovementComponent.cs
+++ Assets/Team20/EmptyGame/Scripts/MovementComponent.cs
@@ -6,17 +6,16 @@ namespace MatrixJam.Team20
 {
     public class MovementComponent : MonoBehaviour
     {
-        Rigidbody2D rigidBody;
         bool left = false;
         public float speed = 10f;
         public float jumpSpeed = 10f;
         public float maxSpeed = 10f;
         public bool grounded = true;
+        Vector2 velocity = new Vector2(0f, 0f);
 
         // Start is called before the first frame update
         void Start()
         {
-            rigidBody = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
@@ -35,12 +34,11 @@ namespace MatrixJam.Team20
                 left = true;
             }
 
+            velocity.x = Horizontal * speed * Time.deltaTime;
             if (grounded)
             {
-                rigidBody.velocity = new Vector3(0, rigidBody.velocity.y);
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    rigidBody.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
                     grounded = false;
                 }
                 else
@@ -50,8 +48,7 @@ namespace MatrixJam.Team20
             }
             else
             {
-                rigidBody.AddForce(Horizontal * Vector2.right * speed);
-                rigidBody.velocity = new Vector3(Mathf.Clamp(rigidBody.velocity.x, -maxSpeed, maxSpeed), rigidBody.velocity.y);
+                
             }
         }
     }
