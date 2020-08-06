90cb30df6c9c3a06da8194e8ee7644473418d7fd
ido <games@ido.wtf>
2020-08-06 19:58:14 +0300

    Example Commit

---
+++ Assets/Team5/ExampleGame/Scripts/ExampleGameBallController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatrixJam.Team5
+{
+    public class ExampleGameBallController : MonoBehaviour
+    {
+
+        [SerializeField] private float Speed = 1;
+
+        [SerializeField] private Vector3 inputVector;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            inputVector.x = Input.GetAxis("Horizontal");
+            inputVector.z = Input.GetAxis("Vertical");
+            gameObject.GetComponent<Rigidbody>().AddForce(inputVector * Speed);
+        }
+    }
+}
---
+++ Assets/Team5/ExampleGame/Scripts/ExampleGameStartHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MatrixJam;
+using UnityEngine;
+
+namespace MatrixJam.Team5
+{
+    public class ExampleGameStartHelper : StartHelper
+    {
+
+        public override void StartHelp(int num_ent)
+        {
+            // this is how the game starts
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
