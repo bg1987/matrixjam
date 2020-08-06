d971aa31b529953ef0408c5088aef47b072a9bcc
Eylon <eylon.keshet@gmail.com>
2020-08-06 22:25:11 +0300

    Eylon init push

---
+++ Assets/Team20/EmptyGame/Scripts/EmptryGameStartHelper.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using MatrixJam;
+using UnityEngine;
+
+namespace MatrixJam.Team20
+{
+    public class EmptyGameStartHelper : StartHelper
+    {
+       
+    public override void StartHelp(int num_ent)
+    {
+        // this is how the game starts
+        Debug.Log("Player Entered through entrance number:" + num_ent);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    }
+}
