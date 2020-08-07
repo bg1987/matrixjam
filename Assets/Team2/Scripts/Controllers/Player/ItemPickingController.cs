cc2d8b9629324dd803f5017993005359ce863599
fakemelon <oren.israel5@gmail.com>
2020-08-07 15:49:38 +0300

    Can now rotate held objects.

--- Assets/Team2/Scripts/Controllers/Player/ItemPickingController.cs
+++ Assets/Team2/Scripts/Controllers/Player/ItemPickingController.cs
@@ -6,11 +6,15 @@ namespace MatrixJam.Team2
 {
     public class ItemPickingController : MonoBehaviour
     {
+        [SerializeField] 
+        [Range(0f, 1f)]
+        private float rotateSpeed;
+
         private Pickable pickable;
 
         private void Update()
         {
-            Vector2 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             if (Input.GetMouseButtonDown(0))
             {
@@ -20,24 +24,26 @@ namespace MatrixJam.Team2
                 }
                 else
                 {
-                    Pickup(mousePositionInWorld);
+                    Pickup(mousePosition);
                 }
             }
 
             if (IsPickablePicked())
             {
-                MovePickable(mousePositionInWorld);
+                MovePickable(mousePosition);
+                RotatePickable(mousePosition);
             }
         }
 
-        public void Pickup(Vector2 mousePositionInWorld)
+        public void Pickup(Vector2 mousePosition)
         {
-            RaycastHit2D raycastHit = Physics2D.Raycast(mousePositionInWorld, Vector2.zero, 0f);
+            RaycastHit2D raycastHit = Physics2D.Raycast(mousePosition, Vector2.zero, 0f);
             if (raycastHit.collider != null)
             {
                 Pickable pickable;
                 if (raycastHit.collider.TryGetComponent(out pickable))
                 {
+                    pickable.Pick();
                     this.pickable = pickable;
                 }
             }
@@ -50,12 +56,25 @@ namespace MatrixJam.Team2
 
         private void DropPickable()
         {
+            pickable.Drop();
             pickable = null;
         }
 
-        private void MovePickable(Vector2 mousePositionInWorld)
+        private void MovePickable(Vector2 mousePosition)
         {
-            pickable.transform.position = mousePositionInWorld;
+            pickable.transform.position = mousePosition;
+        }
+
+        private void RotatePickable(Vector2 mousePosition)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                pickable.transform.Rotate(0, 0, rotateSpeed);
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                pickable.transform.Rotate(0, 0, -rotateSpeed);
+            }
         }
     }
 }
