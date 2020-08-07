7681038e1beb232a984da2a07bb62fba1c447ef3
galsa3 <galsa3@gmail.com>
2020-08-07 21:51:47 +0300

    test

--- Assets/Team4/Scripts/IntroManager.cs
+++ Assets/Team4/Scripts/IntroManager.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MatrixJam.Team;
 
-namespace MatrixJam.Team
+
+namespace MatrixJam.Team4
 {
-    public class IntroManager : MonoBehaviour
+    public class IntroManager
     {
-        // Start is called before the first frame update
-        void Start()
+        private UIManager uiManager;
+        private List<MessageObject> _introMessages;
+        private int _msgIndex;
+
+        public IntroManager(List<UIMessage> introMessages)
         {
-            
+            _introMessages = introMessages;
+            _msgIndex = 0;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void nextMessage()
         {
-            
+            ++_msgIndex;
+            if(_msgIndex < _introMessages.Count())
+            {
+                uiManager.ShowIntroMessage(_introMessages.IndexOf(_msgIndex));
+                return;
+            } 
+
+            // TODO fire event
         }
+
+        public void previousMessage()
+        {
+            if(0 < _msgIndex)
+            {
+                --_msgIndex;
+                uiManager.ShowIntroMessage(_introMessages.IndexOf(_msgIndex));
+            }
+        }
+
     }
 }
