a0750e6dd4d374a4b5ec9ab25d81d18e895aa6b6
Jonathan Levin <jonmlevin@outlook.com>
2020-08-07 18:02:18 +0300

    Add Beat Debug Display

---
+++ Assets/Team14/Scripts/BeatDebugDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace MatrixJam.Team14
+{
+    public class BeatDebugDisplay : MonoBehaviour
+    {
+        [SerializeField] private GameManager gameManager;
+        [SerializeField] private Camera cam;
+
+        [Header("Colors")]
+        [SerializeField] private Color[] beatColors = new Color[4];
+
+        [Header("Sizes")]
+        [SerializeField] private float[] beatSizes = new float[4];
+
+        private int beatsPerBar = 4;
+        
+        private void OnDrawGizmos()
+        {
+            if (!cam || !gameManager) return;
+            
+            var beatPositions = gameManager.BeatPositions;
+            for (var i = 0; i < beatPositions.Length; i++)
+            {
+                var beatPos = beatPositions[i];
+                DrawLine(beatPos, i);
+            }
+        }
+
+        private void DrawLine(Vector3 center, int beatIdx)
+        {
+            var i = beatIdx % beatsPerBar; // The  beats index in the bar
+            
+            Gizmos.color = beatColors[i];
+            var halfSize = beatSizes[i] * 0.5f;
+            
+            Gizmos.DrawLine(
+                center + Vector3.up * halfSize, 
+                center - Vector3.up * halfSize
+            );
+        }
+    }
+}
--- Assets/Team14/Scripts/GameManager.cs
+++ Assets/Team14/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using BezierSolution;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace MatrixJam.Team14
@@ -6,12 +8,13 @@ namespace MatrixJam.Team14
     public class GameManager : MonoBehaviour
     {
         // If the audio is a loop - How many times do we expect it to repeat? (For spline pos)
-        [SerializeField] private float loopCount;
+        [SerializeField] private float bpm;
+        [SerializeField] private float xPerBeat;
 
-        [SerializeField] private float splineNormalizedStart;
-        // [SerializeField] private float splineNormalizedEnd = 1.0f;
+        [SerializeField] private Transform startAndDirection;
+        // [SerializeField] private float splineEnd = 1.0f;
 
-        [SerializeField] private BezierSpline mainSpline;
+        // [SerializeField] private BezierSpline mainSpline;
         [SerializeField] private Transform character;
         [SerializeField] private AudioSource mainAudio;
 
@@ -20,24 +23,52 @@ namespace MatrixJam.Team14
         [SerializeField] private Exit loseExit;
 
         private bool reachedEnd;
-        private int _audioLoopCounter;
+        // private int _audioLoopCounter;
         private float _prevAudioProgress;
+        private float _prevY;
 
-        private float ProgressPerLoop => (1.0f - splineNormalizedStart) / loopCount;
+        public float BeatPerSec => bpm / 60;
+        public float XPerSec => xPerBeat * BeatPerSec;
 
+        public Vector3[] BeatPositions { get; private set; }
+
+        private void Awake()
+        {
+            // Can maybe get rid of this if it causes problems
+            UpdateBeatPositions();    
+        }
+        
         private void Update()
         {
             if (reachedEnd) return;
 
             var audioProgress = GetAudioProgress(mainAudio);
-            if (_prevAudioProgress > audioProgress) _audioLoopCounter++;
+            // if (_prevAudioProgress > audioProgress) _audioLoopCounter++;
 
-            var t = GetPlayerProgress(_audioLoopCounter + audioProgress);
-            var pos = mainSpline.GetPoint(t);
+            // var pos = GetPosition(_audioLoopCounter, mainAudio);
+            var pos = GetPosition(mainAudio);
             character.position = pos;
-
-            if (t >= 1.0f) OnReachedEnd();
+            
+            // if ((pos - FinalPoint).sqrMagnitude < 0.1f) OnReachedEnd();
+            
+            
+            _prevY = pos.y;
             _prevAudioProgress = audioProgress;
+        }
+
+        private void OnValidate()
+        {
+            UpdateBeatPositions();
+        }
+
+        private void UpdateBeatPositions()
+        {
+            var totalBeats = mainAudio.clip.length * BeatPerSec;
+
+            BeatPositions = Enumerable
+                .Range(0, Mathf.CeilToInt(totalBeats))
+                .Select(i => GetPosition(i))
+                .ToArray();
         }
 
         private void OnReachedEnd()
@@ -46,12 +77,23 @@ namespace MatrixJam.Team14
             Debug.Log("End!");
         }
 
-        private float GetPlayerProgress(float loops)
+        private Vector3 GetPosition(AudioSource source)
         {
-            var progress = loops * ProgressPerLoop;
-            // Mathf.Clamp(progress, splineNormalizedStart, splineNormalizedEnd);
-            return splineNormalizedStart + progress;
+            // var loopedSecs = currLoops * source.clip.length;
+            // var totalSeces = loopedSecs + source.time;
+            var totalSecs = source.time;
+            var beat = GetBeatNum(totalSecs);
+
+            return GetPosition(beat);
         }
+        
+        private Vector3 GetPosition(float beat)
+        {
+            var x = beat * xPerBeat;
+            return startAndDirection.position + startAndDirection.right * x;
+        }
+
+        private float GetBeatNum(float secs) => secs * BeatPerSec;
 
         private static float GetAudioProgress(AudioSource audioSource)
             => audioSource.time / audioSource.clip.length;
