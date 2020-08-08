ea9611eaf5853300f1e70d260a9467821669c545
fakemelon <oren.israel5@gmail.com>
2020-08-08 20:47:24 +0300

    Added checkpoints

---
+++ Assets/Team2/Scripts/Checkpoint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatrixJam.Team2
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class Checkpoint : MonoBehaviour
+    {
+    }
+}
