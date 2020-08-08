8a995ddfed6820fdd8db80840c7d523b6ac872db
oded <oded56@gmail.com>
2020-08-09 00:00:11 +0300

    Updated turret

--- Assets/Team2/Scripts/Controllers/Gun/RotateTowardsPoint.cs
+++ Assets/Team2/Scripts/Controllers/Gun/RotateTowardsPoint.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace MatrixJam.Team2
-    {
-        namespace MatrixJam.Team2
 {
-    public class RotateTowardsPoint : MonoBehaviour
+    namespace MatrixJam.Team2
+    {
+        public class RotateTowardsPoint : MonoBehaviour
         {
             [SerializeField] private Transform target;
-
-            private Vector3 offset;
-
-            void Start()
-            {
-                offset = new Vector3(transform.position.x - transform.parent.position.x, 0, 0);
-            }
+            [SerializeField] private float correction;
 
             void Update()
             {
-                Vector3 targetPos = target.position;
+                var aimDir = target.position - transform.position; 
 
-                Vector3 aimDir = (targetPos - transform.parent.position);
-                float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-                transform.position = transform.parent.position + transform.rotation * offset;
+                float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg - correction;
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
             }
         }
     }
