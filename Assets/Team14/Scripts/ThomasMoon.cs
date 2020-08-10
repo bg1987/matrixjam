f079c098c331523d7a10ff8c387b8e3a52a4dd28
Jonathan Levin <jon.levin25@gmail.com>
2020-08-10 17:31:29 +0300

    Thomas + anims (todo: route player to them)

--- Assets/Team14/Scripts/ThomasMoon.cs
+++ Assets/Team14/Scripts/ThomasMoon.cs
@@ -1,53 +1,172 @@
+using System;
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace MatrixJam.Team14
 {
     public class ThomasMoon : MonoBehaviour
     {
+        public enum LookAt
+        {
+            Cursor,
+            Target
+        }
+        
         [SerializeField] private Camera cam;
         [SerializeField] private float localEyeRadius;
         [SerializeField] private float scalePerSec;
 
+        [SerializeField] private Animator anim;
         [SerializeField] private SpriteRenderer faceSprite;
         [SerializeField] private SpriteRenderer leftEye;
         [SerializeField] private SpriteRenderer rightEye;
         [SerializeField] private SpriteRenderer leftEyebrow;
         [SerializeField] private SpriteRenderer rightEyebrow;
-
-        public bool lookAtCursor;
-        public Transform LookAtTransform;
+        [SerializeField] private LookAt lookMode;
+        [SerializeField] private Transform target;
 
         public float Z => faceSprite.transform.position.z;
 
-        private Vector3 LookAt
+        private bool _ignoreEyes;
+        private const string TrigWeird = "weird";
+        private const string TrigEyebrows = "eyebrows";
+        private const string TrigHappy = "happy";
+
+        public LookAt LookMode
+        {
+            get => lookMode;
+            set => lookMode = value;
+        }
+
+        public Transform Target
+        {
+            get => target;
+            set => target = value;
+        }
+
+        private Vector3? LookAtPos
         {
             get
             {
-                if (lookAtCursor)
+                if (_ignoreEyes) return null;
+                
+                switch (LookMode)
                 {
-                    var screenPos = Input.mousePosition;
-                    screenPos.z = Z;
-                    return cam.ScreenToWorldPoint(screenPos);
+                    case LookAt.Cursor:
+                        var screenPos = Input.mousePosition;
+                        screenPos.z = Z;
+                        return cam.ScreenToWorldPoint(screenPos);
+                    case LookAt.Target:
+                        return Target.position;
+                    default:
+                        throw new ArgumentOutOfRangeException();
                 }
-
-                return LookAtTransform.position;
             }
         }
 
         private Vector3 startScale;
 
-        private void Start()
+        private IEnumerator Start()
         {
             startScale = transform.localScale;
+            StartCoroutine(RandomEyebrows(3f, 3f));
+            yield return new WaitForSeconds(1);
+            // HappyAnim(3f);
         }
 
         private void Update()
         {
             transform.localScale += Vector3.one * Time.deltaTime * scalePerSec;
-            var lookAt = LookAt;
+            HandleEyesLookAt();
+        }
+
+        private IEnumerator RandomEyebrows(float minTime, float maxTime, bool startImmediate = false)
+        {
+            if (startImmediate) EyebrowsAnim();
+            while (true)
+            {
+                var randTime = Random.Range(minTime, maxTime); 
+                yield return new WaitForSeconds(randTime);
+                EyebrowsAnim();
+            }
+        }
+
+        public void WeirdAnim(float time)
+        {
+            StartCoroutine(WierdRoutine());
+            IEnumerator WierdRoutine()
+            {
+                StartAnim(TrigWeird, false);
+                yield return new WaitForSeconds(time);
+                EndAnim(TrigWeird);
+            }
+        }
+        
+        public void HappyAnim(float time)
+        {
+            StartCoroutine(HappyRoutine());
+            IEnumerator HappyRoutine()
+            {
+                StartAnim(TrigHappy, false);
+                yield return new WaitForSeconds(time);
+                EndAnim(TrigHappy);
+            }
+        }
+
+        public void EyebrowsAnim()
+        {
+            const float dur1 = 0.4f;
+            const float delay = 1f;
+            const float dur2 = 0.6f;
+
+            var oldLeft = leftEye.transform.localPosition;
+            var oldRight = rightEye.transform.localPosition;
+            var leftTween1 = leftEye.transform.DOLocalMove(Vector3.zero, dur1);
+            var rightTween1 = rightEye.transform.DOLocalMove(Vector3.zero, dur1);
+            
+            var leftTween2 = leftEye.transform.DOLocalMove(oldLeft, dur2);
+            var rightTween2 = rightEye.transform.DOLocalMove(oldRight, dur2);
+            
+
+            var seq = DOTween.Sequence()
+                .Append(leftTween1)
+                .Join(rightTween1)
+                .AppendInterval(delay)
+                .Append(leftTween2)
+                .Join(rightTween2);
+            
+            seq.onComplete = () => EndAnim(TrigEyebrows);
+
+            Debug.Log("EYEBROWS!");
+            StartAnim(TrigEyebrows, true);
+            seq.Play();
+        }
+
+        private void StartAnim(string trig, bool ignoreEyes)
+        {
+            _ignoreEyes = ignoreEyes;
+            anim.SetBool(trig, true);
+        }
+
+        private void EndAnim(string trig)
+        {
+            _ignoreEyes = false;
+            anim.SetBool(trig, false);
+        }
+
+        private bool HandleEyesLookAt()
+        {
+            var lookAtNullable = LookAtPos;
+
+            if (!lookAtNullable.HasValue) return false;
+            var lookAt = lookAtNullable.Value;
             
             EyeLookAt(leftEye, lookAt);
             EyeLookAt(rightEye, lookAt);
+            
+            return true;
         }
 
         public void RestartSize()
