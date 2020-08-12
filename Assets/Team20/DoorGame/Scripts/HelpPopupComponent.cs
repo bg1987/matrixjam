23452b7517c222c357917f446d8db33c375b9591
Eylon <eylon.keshet@gmail.com>
2020-08-12 19:05:52 +0300

    Improved helper window, fixed sfx not going through mixer

--- Assets/Team20/DoorGame/Scripts/HelpPopupComponent.cs
+++ Assets/Team20/DoorGame/Scripts/HelpPopupComponent.cs
@@ -6,11 +6,13 @@ namespace MatrixJam.Team20
 {
     public class HelpPopupComponent : MonoBehaviour
     {
+        public Canvas pressHWindow;
         Canvas canvas;
         // Start is called before the first frame update
         void Start()
         {
             canvas = this.GetComponent<Canvas>();
+            pressHWindow.enabled = false;
         }
 
         // Update is called once per frame
@@ -21,11 +23,13 @@ namespace MatrixJam.Team20
                 if(Input.anyKeyDown)
                 {
                     canvas.enabled = false;
+                    pressHWindow.enabled = true;
                 }
             }
             else if(Input.GetKeyDown(KeyCode.H))
             {
                 canvas.enabled = true;
+                pressHWindow.enabled = false;
             }
         }
     }
