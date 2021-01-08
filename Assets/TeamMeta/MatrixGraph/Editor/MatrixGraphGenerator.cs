0ff9e1704423aad6a7f336cdf09e5e8400927e8b
Or S <ors373@gmail.com>
2021-01-08 13:48:12 +0200

    Makes MatrixGraphGenerator write dummy data when it creates a .matrixgraph

--- Assets/TeamMeta/MatrixGraph/Editor/MatrixGraphGenerator.cs
+++ Assets/TeamMeta/MatrixGraph/Editor/MatrixGraphGenerator.cs
@@ -33,14 +33,59 @@ namespace MatrixJam.TeamMeta
             string path = EditorUtility.SaveFilePanel("Create a new Matrix Graph", lastUsedPath, "NewMatrixGraph", "matrixgraph");
             if (path.Length != 0)
             {
+                MatrixGraphSO graphData = CreateDummyScriptableObject();
+                string graphDataJson = JsonUtility.ToJson(graphData);
+
                 using (FileStream fileStream = new FileStream(path, FileMode.Create))
                 {
+                    using (StreamWriter writer = new StreamWriter(fileStream))
+                    {
+                        writer.WriteLine(graphDataJson);
+                    }
                 }
                 AssetDatabase.Refresh();
                 lastUsedPath = Path.GetDirectoryName(path);
             }
-
             return;
+        }
+        private MatrixGraphSO CreateDummyScriptableObject()
+        {
+            MatrixGraphSO matrixGraphSO = CreateInstance<MatrixGraphSO>();
+
+            var nodes = new List<MatrixNodeData>();
+
+            var node1 = new MatrixNodeData(0, "Level 1");
+            node1 .AddInputPort(0);
+            node1 .AddOutputPort(0);
+            node1.AddOutputPort(3);
+            nodes.Add(node1);
+
+            var node2 = new MatrixNodeData(1, "Level 2");
+            node2.AddInputPort(0);
+            node2.AddInputPort(1);
+            node2.AddOutputPort(0);
+            node2.AddOutputPort(1);
+            node2.AddOutputPort(2);
+            nodes.Add(node2);
+
+            var node3 = new MatrixNodeData(2, "Level 3");
+            node3.AddInputPort(0);
+            node3.AddInputPort(1);
+            node3.AddOutputPort(4);
+            node3.AddOutputPort(7);
+            nodes.Add(node3);
+
+            var node4 = new MatrixNodeData(3, "Level 4");
+            node4.AddInputPort(0);
+            node4.AddOutputPort(0);
+            nodes.Add(node4);
+
+            var edges = new List<MatrixEdgeData>();
+
+            matrixGraphSO.nodes = nodes;
+            matrixGraphSO.edges = edges;
+
+            return matrixGraphSO;
         }
     }
 }
