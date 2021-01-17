9c5a9b2441b692eff4a40f2c18c1f3840979dc2f
Or S <ors373@gmail.com>
2021-01-17 21:28:47 +0200

    Creates a NewGame, Continue, and Quit buttons in StartScene

---
+++ Assets/TeamMeta/Scripts/MainMenu/QuitApplication.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatrixJam.TeamMeta
+{
+    public class QuitApplication : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
