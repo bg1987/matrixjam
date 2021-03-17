201f4d6000834124a42aa46dd0c99358d4955d41
Or S <ors373@gmail.com>
2021-03-17 13:40:56 +0200

    Implements MatrixMap Edge's BezierCurve, also its editor visualization

--- Assets/TeamMeta/MatrixMap/Edge/Edge.cs
+++ Assets/TeamMeta/MatrixMap/Edge/Edge.cs
@@ -6,6 +6,12 @@ namespace MatrixJam.TeamMeta.MatrixMap
 {
     public class Edge : MonoBehaviour
     {
+        [SerializeField] int curveResolution = 9;
+
+        [SerializeField] List<Vector3> anchorPoints = new List<Vector3>() { Vector3.zero, new Vector3(0.5f, 0.5f, 0f), Vector3.right };
+        List<Vector3> curvePoints;
+        float edgeLength;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -16,6 +22,69 @@ namespace MatrixJam.TeamMeta.MatrixMap
         void Update()
         {
             
+        }
+        Vector3 QuadraticBezierCurve(Vector3 p1, Vector3 p2, Vector3 p3, float t)
+        {
+            t = Mathf.Clamp01(t);
+            float oneMinusT = 1f - t;
+            return
+                oneMinusT * oneMinusT * p1 +
+                2f * oneMinusT * t * p2 +
+                t * t * p3;
+        }
+        public List<Vector3> CalculateBezierDistributedPoints(Vector3 p1, Vector3 p2, Vector3 p3, int resolution)
+        {
+            curvePoints.Clear();
+
+            curvePoints.Add(p1);
+
+            //Start QuadraticBezierCurveLength -> For optimization - Put here to avoid doing QuadraticBezierCurve twice.
+            float length = 0;
+            Vector3 previousPoint = p1;
+            //QuadraticBezierCurveLength
+
+            for (int i = 1; i < resolution; i++)
+            {
+                Vector3 point = QuadraticBezierCurve(p1, p2, p3, i / ((float)resolution - 1));
+                curvePoints.Add(point);
+
+                //QuadraticBezierCurveLength
+                length += Vector3.Distance(previousPoint, point);
+                previousPoint = point;
+                //QuadraticBezierCurveLength
+            }
+            //QuadraticBezierCurveLength
+            edgeLength = length;
+            //End QuadraticBezierCurveLength
+
+            curvePoints[curvePoints.Count - 1] = p3;
+            return curvePoints;
+        }
+        public void Init(Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            curvePoints = CalculateBezierDistributedPoints(p1, p2, p3, curveResolution);
+
+            anchorPoints[0] = p1;
+            anchorPoints[1] = p2;
+            anchorPoints[2] = p3;
+        }
+        public void UpdateBezierCurve(Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            anchorPoints[0] = p1;
+            anchorPoints[1] = p2;
+            anchorPoints[2] = p3;
+
+            curvePoints = CalculateBezierDistributedPoints(p1, p2, p3, curveResolution);
+        }
+        public List<Vector3> GetAnchorPoints()
+        {
+            List<Vector3> anchorPointsCopy = new List<Vector3>(anchorPoints);
+            return anchorPointsCopy;
+        }
+        public List<Vector3> GetCurvePoints()
+        {
+            List<Vector3> pointsCopy = new List<Vector3>(curvePoints);
+            return pointsCopy;
         }
     }
 }
---
+++ Assets/TeamMeta/MatrixMap/Edge/Editor/EdgeEditor.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace MatrixJam.TeamMeta.MatrixMap
+{
+    [CustomEditor(typeof(Edge))]
+
+    public class EdgeEditor : Editor
+    {
+        private Edge edge;
+        List<Vector3> anchorPoints;
+        List<Vector3> bezierCurvePoints;
+
+        static float handlesSize = 0.2f;
+        static bool showPointsAlongCurve = false;
+        static float pointsAlongCurveSize = 0.05f;
+
+        private void OnEnable()
+        {
+            edge = target as Edge;
+            anchorPoints = edge.GetAnchorPoints();
+
+            bezierCurvePoints = edge.GetCurvePoints();
+
+            edge.UpdateBezierCurve(anchorPoints[0], anchorPoints[1], anchorPoints[2]);
+            bezierCurvePoints = edge.GetCurvePoints();
+
+        }
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            //EditorGUILayout.Separator();
+            DrawHeaderInspector("Editor");
+
+            handlesSize = EditorGUILayout.FloatField("Handles Size", handlesSize);
+
+            showPointsAlongCurve = EditorGUILayout.Toggle("Show Points Along Curve", showPointsAlongCurve);
+            pointsAlongCurveSize = EditorGUILayout.FloatField("points Along Curve Size", pointsAlongCurveSize);
+
+        }
+
+        private static void DrawHeaderInspector(string label)
+        {
+            var headerSkin = GUI.skin.label;
+            headerSkin.fontStyle = FontStyle.Bold;
+            EditorGUILayout.LabelField(label, headerSkin);
+        }
+
+        protected virtual void OnSceneGUI()
+        {
+            Handles.color = Color.blue;
+
+            for (int i = 0; i < anchorPoints.Count; i++)
+            {
+                Vector3 point = Handles.FreeMoveHandle(anchorPoints[i] + edge.transform.position, Quaternion.identity, handlesSize, Vector3.zero, Handles.ConeHandleCap);
+                if (point != anchorPoints[i])
+                {
+
+                    anchorPoints[i] = point - edge.transform.position;
+                    edge.UpdateBezierCurve(anchorPoints[0], anchorPoints[1], anchorPoints[2]);
+
+                    bezierCurvePoints = edge.GetCurvePoints();
+                }
+            }
+            Handles.color = Color.gray;
+            Handles.DrawLine(anchorPoints[0] + edge.transform.position, anchorPoints[1] + edge.transform.position);
+            Handles.DrawLine(anchorPoints[2] + edge.transform.position, anchorPoints[1] + edge.transform.position);
+
+            DrawBezierCurve();
+
+            if (showPointsAlongCurve)
+                DrawPointsAlongCurve();
+        }
+        void DrawBezierCurve()
+        {
+            Handles.color = Color.white;
+            for (int i = 1; i < bezierCurvePoints.Count; i++)
+            {
+                Vector3 p1 = bezierCurvePoints[i - 1] + edge.transform.position;
+                Vector3 p2 = bezierCurvePoints[i] + edge.transform.position;
+                Handles.DrawLine(p1, p2);
+            }
+
+        }
+        void DrawPointsAlongCurve()
+        {
+            Handles.color = Color.gray;
+            for (int i = 0; i < bezierCurvePoints.Count; i++)
+            {
+                Handles.SphereHandleCap(i, bezierCurvePoints[i] + edge.transform.position, Quaternion.identity, pointsAlongCurveSize, EventType.Repaint);
+            }
+        }
+    }
+}
