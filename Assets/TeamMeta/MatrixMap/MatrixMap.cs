95475e6d174e8f66c66eff42d818b05f6237cacf
Or S <ors373@gmail.com>
2021-03-18 02:05:38 +0200

    Implements functionality to create nodes in a radius in MatrixMap

--- Assets/TeamMeta/MatrixMap/MatrixMap.cs
+++ Assets/TeamMeta/MatrixMap/MatrixMap.cs
@@ -6,16 +6,62 @@ namespace MatrixJam.TeamMeta.MatrixMap
 {
     public class MatrixMap : MonoBehaviour
     {
+        [SerializeField] Node nodePrefab;
+        [SerializeField] int nodesCount;
+
+        [SerializeField] float nodesSize = 1;
+        [SerializeField] float radius = 1;
+
+        List<Node> nodes = new List<Node>();
+        List<Vector3> nodesPositions = new List<Vector3>();
+
+        const float TAU = Mathf.PI * 2;
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            InitMap();
         }
 
         // Update is called once per frame
         void Update()
         {
             
+        }
+        void InitMap()
+        {
+            CalculateNodesPositions();
+            CreateNodes();
+            //UpdateNodesScale();
+            //UpadteNodesRadius();
+
+            //DebugVisualizeFirstLastNodes();
+        }
+        private List<Vector3> CalculateNodesPositions()
+        {
+            nodesPositions.Clear();
+
+            float rotateOffset = -Mathf.PI / 2f; //90 degrees. For visual symmetery
+
+            for (int i = 0; i < nodesCount; i++)
+            {
+                float t = i / (float)nodesCount;
+                float rotateBy = t * TAU + rotateOffset;
+                nodesPositions.Add(new Vector3(Mathf.Cos(rotateBy), Mathf.Sin(rotateBy), transform.position.z) * radius);
+            }
+            return nodesPositions;
+        }
+        void CreateNodes()
+        {
+            for (int i = 0; i < nodesCount; i++)
+            {
+                var node = Instantiate(nodePrefab, transform);
+                node.transform.position = nodesPositions[i];
+                node.name = "Node"+i;
+                node.transform.localScale = Vector3.one * nodesSize;
+                nodes.Add(node);
+            }
+
         }
     }
 }
--- Assets/TeamMeta/MatrixMap/Node/Node.cs
+++ Assets/TeamMeta/MatrixMap/Node/Node.cs
@@ -6,6 +6,8 @@ namespace MatrixJam.TeamMeta.MatrixMap
 {
     public class Node : MonoBehaviour
     {
+        [SerializeField] GameObject model;
+
         // Start is called before the first frame update
         void Start()
         {
