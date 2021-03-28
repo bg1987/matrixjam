cac7d9b24ed26dd1e159919447d22db9dadc0f7a
Or S <ors373@gmail.com>
2021-03-29 01:33:41 +0300

    Makes a ColorHdr class to seperate intensity from color Will be used to animate intensity

---
+++ Assets/TeamMeta/MatrixMap/ColorHdr/ColorHdr.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatrixJam.TeamMeta
+{
+    [System.Serializable]
+    public struct ColorHdr
+    {
+        public Color color;
+        public float intensity;
+    }
+}
